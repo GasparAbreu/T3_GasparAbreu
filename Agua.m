c7387e59fb647837d436823f4cefea26c8e95cee
GasparAbreu <43260025+GasparAbreu@users.noreply.github.com>
2018-10-03 18:35:51 -0500

    Add files via upload

---
+++ Agua.m
@@ -0,0 +1,22 @@
+prompt=('Ingrese una cantidad: ');
+Cantidad=input(prompt);
+if Cantidad <=50
+    fprintf('La cuota mínima es:')
+    Pago=150
+else
+    if Cantidad >50 && Cantidad <200
+        fprintf('El precio de los litros pedidos es:')
+        Pago=(Cantidad-50)*3  
+        if Pago <150
+            PagoReal=150
+        end
+    else
+        if Cantidad >=200
+        fprintf('El precio de los litros pedidos es:')
+        Pago=((Cantidad-200)*5+(150*3))
+        if Pago <150
+            PagoReal=150
+        end
+        end
+    end
+end
